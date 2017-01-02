51486968684c29154b37264ad0e472f8d259648b
Enrique Barbeito García <ebarbeito@gmail.com>
2017-01-02 19:57:46 +0100

    Add with empty string should return 0

--- src/StringCalculator.php
+++ src/StringCalculator.php
@@ -4,4 +4,8 @@ namespace KataStringCalculator;
 
 class StringCalculator
 {
+    public static function Add(string $string): int
+    {
+        return 0;
+    }
 }
--- test/StringCalculatorTest.php
+++ test/StringCalculatorTest.php
@@ -2,10 +2,12 @@
 
 namespace KataStringCalculator\Test;
 
+use KataStringCalculator\StringCalculator;
+
 class StringCalculatorTest extends \PHPUnit_Framework_TestCase
 {
-    public function test_dummy()
+    public function test_add_with_empty_string_should_return_0()
     {
-        $this->assertEquals(1, 1);
+        $this->assertEquals(0, StringCalculator::Add(''));
     }
 }
