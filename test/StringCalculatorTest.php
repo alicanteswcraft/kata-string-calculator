be633addda0ffdb62b3d922c9f5e166037747530
Enrique Barbeito García <ebarbeito@gmail.com>
2017-01-02 19:53:27 +0100

    First commit

---
+++ test/StringCalculatorTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace KataStringCalculator\Test;
+
+class StringCalculatorTest extends \PHPUnit_Framework_TestCase
+{
+    public function test_dummy()
+    {
+        $this->assertEquals(1, 1);
+    }
+}
